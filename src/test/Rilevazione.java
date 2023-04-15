3f0edb010980db612201e7d7f05cfeec16ab4d7c
Scremmix <57624331+Scremmix@users.noreply.github.com>
2023-04-15 11:39:53 +0200

    Creata RilevazioneException, lanciata da RILEVAZIONE nel caso in cui nessuna area sia stata selezionata

--- src/test/Rilevazione.java
+++ src/test/Rilevazione.java
@@ -9,15 +9,17 @@ import java.util.Scanner;
 
 public class Rilevazione {
     private String centro; //esempio: Insubria
-    private long area; //esempio: 3178229 (ID per l'area di Como, IT)
+    private Long area=Long.valueOf(0); //esempio: 3178229 (ID per l'area di Como, IT)
     private int tipoDato, valore; //esempio: 3=Temperatura, 4 valore
     private String nota;
     private String data, ora;
     
-    public Rilevazione(String centro, long area, int tipoDato, int valore, String nota)
+    public Rilevazione(String centro, Long area, int tipoDato, int valore, String nota) throws rilevazioneException
     {
         this.centro=centro;
         this.area=area;
+        if(this.area==null||this.area<0)
+            throw new rilevazioneException("Nessuna area selezionata.");
         this.tipoDato=tipoDato;
         this.valore=valore;
         this.nota=nota;
@@ -31,7 +33,7 @@ public class Rilevazione {
     public Rilevazione()
     {
         centro="";
-        area=0;
+        area=Long.valueOf(0);
         tipoDato=-1;
         valore=0;
         nota="";
@@ -45,7 +47,7 @@ public class Rilevazione {
     public void SalvaRilevazione(){
         try (FileWriter writer = new FileWriter("../datafiles/ParametriClimatici.csv",true)) {
             writer.write(
-                    centro+"#"+area+"#"+data+"#"+ora+"#"+tipoDato+"#"+valore+"#"+nota+"\n"
+                    centro+"#"+area.toString()+"#"+data+"#"+ora+"#"+tipoDato+"#"+valore+"#"+nota+"\n"
                 );
             writer.close();
         }
